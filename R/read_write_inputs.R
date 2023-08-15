d64452f905499e09ed1595630b53a063ee3fa0b5
Christoph Schürz <christoph.schuerz@ufz.de>
2023-08-15 16:06:15 +0200

    Add update_landuse()
    - Add function to perform land use changes
    - Add files which must be read from SWAT+ inputs
    - Generalize read_mgt to read_tbl2

--- R/read_write_inputs.R
+++ R/read_write_inputs.R
@@ -9,19 +9,37 @@
 #'
 read_swat_inputs <- function(project_path) {
   input_list <- list(
-    object.cnt     = read_tbl(paste0(project_path, '/object.cnt')),
-    landuse.lum    = read_tbl(paste0(project_path, '/landuse.lum')),
-    management.sch = read_mgt_sch(paste0(project_path, '/management.sch')),
-    hru_data.hru   = read_tbl(paste0(project_path, '/hru-data.hru')),
-    hru.con        = read_con_file(paste0(project_path, '/hru.con')),
-    rout_unit.rtu  = read_tbl(paste0(project_path, '/rout_unit.rtu')),
-    rout_unit.con  = read_con_file(paste0(project_path, '/rout_unit.con')),
-    rout_unit.def  = read_tbl(paste0(project_path, '/rout_unit.def')),
-    rout_unit.ele  = read_tbl(paste0(project_path, '/rout_unit.ele')),
-    chandeg.con    = read_con_file(paste0(project_path, '/chandeg.con')),
-    reservoir.res  = read_tbl(paste0(project_path, '/reservoir.res')),
-    hydrology.res  = read_tbl(paste0(project_path, '/hydrology.res')),
-    reservoir.con  = read_con_file(paste0(project_path, '/reservoir.con'))
+    object.cnt        = read_tbl(paste0(project_path, '/object.cnt')),
+    landuse.lum       = read_tbl(paste0(project_path, '/landuse.lum')),
+    cntabe.lum        = read_tbl_n(paste0(project_path, '/cntable.lum'),
+                                   id_col_sel = 1:5, id_num = 2:5),
+    cons_practice.lum = read_tbl_n(paste0(project_path, '/cons_practice.lum'),
+                                id_col_sel = 1:3, id_num = 2:3),
+    ovn_table.lum     = read_tbl_n(paste0(project_path, '/ovn_table.lum'),
+                                   id_col_sel = 1:4, id_num = 2:4),
+    management.sch    = read_tbl2(
+                          file_path = paste0(project_path, '/management.sch'),
+                          def_names = c('name', 'numb_ops', 'numb_auto'),
+                          par_names = c('op_typ', 'mon', 'day', 'hu_sch',
+                                        paste0('op_data', 1:3)),
+                          id_num    =  c(2:3, 5:7, 10)),
+    plant.ini         = read_tbl2(
+                          file_path = paste0(project_path, '/plant.ini'),
+                          def_names = c('pcom_name', 'plt_cnt', 'rot_yr_ini'),
+                          par_names = c('plt_name', 'lc_status', 'lai_init',
+                                        'bm_init', 'phu_init', 'plnt_pop',
+                                        'yrs_init', 'rsd_init'),
+                          id_num    =  c(2:3, 6:11)),
+    hru_data.hru      = read_tbl(paste0(project_path, '/hru-data.hru')),
+    hru.con           = read_con_file(paste0(project_path, '/hru.con')),
+    rout_unit.rtu     = read_tbl(paste0(project_path, '/rout_unit.rtu')),
+    rout_unit.con     = read_con_file(paste0(project_path, '/rout_unit.con')),
+    rout_unit.def     = read_tbl(paste0(project_path, '/rout_unit.def')),
+    rout_unit.ele     = read_tbl(paste0(project_path, '/rout_unit.ele')),
+    chandeg.con       = read_con_file(paste0(project_path, '/chandeg.con')),
+    reservoir.res     = read_tbl(paste0(project_path, '/reservoir.res')),
+    hydrology.res     = read_tbl(paste0(project_path, '/hydrology.res')),
+    reservoir.con     = read_con_file(paste0(project_path, '/reservoir.con'))
   )
 
   return(input_list)
@@ -81,26 +99,118 @@ read_tbl <- function(file_path, col_names = NULL, n_skip = 1) {
   return(tbl)
 }
 
-#' Add a running ID to duplicated names
+#' Read SWAT+ management schedule file.
 #'
-#' @param col_name Character vector of column names
+#' @param file_path Path of the SWAT+ management.sch input file.
 #'
-#' @returns the `col_name` character vector with IDs for duplicated names
+#' @importFrom data.table fread
+#' @importFrom dplyr bind_rows bind_cols mutate %>%
+#' @importFrom purrr map map_int map_chr map2 map2_df map_df set_names
+#' @importFrom readr read_lines
+#' @importFrom stringr str_replace_all str_trim str_split
+#' @importFrom tibble as_tibble
+#'
+#' @returns The SWAT+ management.sch input file as a tibble.
 #'
 #' @keywords internal
 #'
-add_suffix_to_duplicate <- function(col_name){
-  dupl <- table(col_name) %>%
-    .[. > 1]
+read_tbl2 <- function(file_path, def_names, par_names, id_num = NULL) {
+  n_def <- length(def_names)
+  n_par <- length(par_names)
 
-  if(length(dupl > 0)) {
-    for(i in 1:length(dupl)) {
-      col_name[col_name == names(dupl[i])] <-
-        paste0(names(dupl[i]), c('', 1:(dupl[i]-1)))
+  file_line <- fread(file_path, skip = 2, sep = NULL, sep2 = NULL,
+                     header = FALSE) %>%
+    unlist(.) %>%
+    unname(.) %>%
+    str_trim(.) %>%
+    str_replace_all(., '\t', ' ') %>%
+    str_split(., '[:space:]+')
+
+  n_elem <- map_int(file_line, length)
+  file_line  <- file_line[n_elem != 1]
+  n_elem <- n_elem[n_elem != 1]
+  def_pos <- which(n_elem == n_def)
+
+  pos_start <- def_pos + 1
+  pos_end <- c(def_pos[2:length(def_pos)] - 1, length(file_line))
+  no_entry <- pos_start > pos_end
+  pos_start[no_entry] <- length(file_line) + 1
+  pos_end[no_entry] <- length(file_line) + 1
+
+  par_tbl <- map2(pos_start, pos_end, ~ file_line[.x:.y]) %>%
+    map(., unlist) %>%
+    map(., ~ as_mtx_null(.x, n_par)) %>%
+    map(., ~ as_tibble(.x, .name_repair = ~ par_names))
+
+  n_op <- map_int(par_tbl, nrow)
+
+  par_tbl <- bind_rows(par_tbl)
+
+  def_tbl <- map(def_pos, ~ file_line[[.x]]) %>%
+    map(., unlist) %>%
+    map(., ~ as_mtx_null(.x, n_def)) %>%
+    map(., ~ as_tibble(.x, .name_repair = ~ def_names)) %>%
+    map2(., n_op, ~ .x[rep(1, .y), ]) %>%
+    bind_rows(.)
+
+  tbl <- bind_cols(def_tbl, par_tbl)
+
+  if(!is.null(id_num)) {
+    tbl[,id_num] <- map_df(tbl[,id_num], as.numeric)
+  }
+
+  return(tbl)
+}
+
+#' Read SWAT+ management schedule file.
+#'
+#' @param file_path Path of the SWAT+ management.sch input file.
+#'
+#' @importFrom data.table fread
+#' @importFrom dplyr bind_rows bind_cols mutate %>%
+#' @importFrom purrr map map_int map_chr map2 map2_df map_df set_names
+#' @importFrom readr read_lines
+#' @importFrom stringr str_replace_all str_trim str_split
+#' @importFrom tibble as_tibble
+#'
+#' @returns The SWAT+ management.sch input file as a tibble.
+#'
+#' @keywords internal
+#'
+read_tbl_n <- function(file_path, col_names = NULL, n_skip = 1,
+                       id_col_sel = NULL, id_num = NULL) {
+  if (is.null(col_names)) {
+    col_names <- fread(file_path, skip = n_skip, nrows = 1, header = F) %>%
+      unlist(.) %>%
+      unname(.) %>%
+      add_suffix_to_duplicate(.)
+    if (!is.null(id_col_sel)) {
+      col_names <- col_names[id_col_sel]
     }
   }
 
-  return(col_name)
+  file_line <- fread(file_path, skip = n_skip + 1, sep = NULL, sep2 = NULL,
+                     header = FALSE) %>%
+    unlist(.) %>%
+    unname(.) %>%
+    str_trim(.) %>%
+    str_replace_all(., '\t', ' ') %>%
+    str_split(., '[:space:]+')
+
+    if (!is.null(id_col_sel)) {
+      file_line <- map(file_line, ~ .x[id_col_sel])
+    }
+
+  tbl <- file_line %>%
+    map(., unlist) %>%
+    map(., ~ as_mtx_null(.x, length(file_line[[1]]))) %>%
+    map_df(., ~ as_tibble(.x, .name_repair = ~ col_names))
+
+  if(!is.null(id_num)) {
+    tbl[,id_num] <- map_df(tbl[,id_num], as.numeric)
+  }
+
+  return(tbl)
 }
 
 #' Read a SWAT+ connecitivity (*.con) input file.
@@ -148,81 +258,6 @@ read_con_file <- function(file_path) {
   return(con_tbl)
 }
 
-
-#' Read SWAT+ management schedule file.
-#'
-#' @param file_path Path of the SWAT+ management.sch input file.
-#'
-#' @importFrom data.table fread
-#' @importFrom dplyr mutate %>%
-#' @importFrom purrr map map_int map_chr map2 map2_df map_df set_names
-#' @importFrom readr read_lines
-#' @importFrom stringr str_replace_all str_trim str_split
-#' @importFrom tibble as_tibble
-#'
-#' @returns The SWAT+ management.sch input file as a tibble.
-#'
-#' @keywords internal
-#'
-read_mgt_sch <- function(file_path) {
-  schdl <- fread(file_path, skip = 2, sep = NULL, sep2 = NULL, header = F) %>%
-    unlist(.) %>%
-    unname(.) %>%
-    str_trim(.) %>%
-    str_replace_all(., '\t', ' ') %>%
-    str_split(., '[:space:]+')
-
-  n_elem <- map_int(schdl, length)
-  schdl  <- schdl[n_elem != 1]
-  n_elem <- n_elem[n_elem != 1]
-  schdl_def_pos <- which(n_elem == 3)
-
-  schdl_name <- map_chr(schdl_def_pos, ~ schdl[[.x]][1])
-
-  schdl_start <- schdl_def_pos + 1
-  schdl_end <- c(schdl_def_pos[2:length(schdl_def_pos)] - 1, length(schdl))
-  no_entry <- schdl_start > schdl_end
-  schdl_start[no_entry] <- length(schdl) + 1
-  schdl_end[no_entry] <- length(schdl) + 1
-  # schdl_start[no_entry] <- 1e9
-  # schdl_end[no_entry] <- 1e9
-
-  col_names <- c('op_typ', 'mon', 'day', 'hu_sch', paste0('op_data', 1:3))
-
-  schdl_mgt <- map2(schdl_start, schdl_end, ~ schdl[.x:.y]) %>%
-    map(., unlist) %>%
-    map(., as_mtx_null) %>%
-    map(., ~ as_tibble(.x, .name_repair = ~ col_names))
-
-  n_op <- map_int(schdl_mgt, nrow)
-  schdl_name <- rep(schdl_name, n_op)
-
-  schdl_mgt <- schdl_mgt %>%
-    bind_rows(.) %>%
-    mutate(., schedule = schdl_name, .before = 1)
-
-  schdl_mgt[,c(3:5, 8)] <- map_df(schdl_mgt[,c(3:5, 8)], as.numeric)
-
-  return(schdl_mgt)
-}
-
-#' Transform x to a matrix with 7 columns and fill up with NA values
-#'
-#' @param x character vector or NULL
-#'
-#' @keywords internal
-#'
-as_mtx_null <- function(x) {
-  if(is.null(x)) {
-    matrix(rep(NA_character_, 7), ncol = 7)
-  } else {
-    matrix(x, nrow = 7) %>%
-      t(.)
-  }
-}
-
-
-
 write_input_tbl <- function(tbl, file_path, fmt) {
   tbl <- map2_df(tbl, fmt, ~ sprintf(.y, .x))
 
@@ -242,4 +277,42 @@ write_input_tbl <- function(tbl, file_path, fmt) {
   input_file <- c(file_head, col_names, file_lines)
 
   write_lines(input_file, file_path)
+}
+
+#' Add a running ID to duplicated names
+#'
+#' @param col_name Character vector of column names
+#'
+#' @returns the `col_name` character vector with IDs for duplicated names
+#'
+#' @keywords internal
+#'
+add_suffix_to_duplicate <- function(col_name){
+  dupl <- table(col_name) %>%
+    .[. > 1]
+
+  if(length(dupl > 0)) {
+    for(i in 1:length(dupl)) {
+      col_name[col_name == names(dupl[i])] <-
+        paste0(names(dupl[i]), c('', 1:(dupl[i]-1)))
+    }
+  }
+
+  return(col_name)
+}
+
+#' Transform x to a matrix with 7 columns and fill up with NA values
+#'
+#' @param x character vector or NULL
+#' @param n Number of elements
+#'
+#' @keywords internal
+#'
+as_mtx_null <- function(x, n) {
+  if(is.null(x)) {
+    matrix(rep(NA_character_, 7), ncol = n)
+  } else {
+    matrix(x, nrow = n) %>%
+      t(.)
+  }
 }
---
+++ R/update_landuse.R
@@ -0,0 +1,178 @@
+#' Update the landuse of selected HRUs
+#'
+#' The land use ('lu-mgt') in hru-data.hru of the land objects defined by the
+#' `hru_id` is overwritten. The new land use label is defined by `type`. For
+#' `type` a new land use is defined in landuse.lum if no land use with this name
+#' already exists in landuse.lum which has the exact same parametrization as
+#' defined with the additional input arguments. If an identical landuse already
+#' exists, this landuse is assigned to the HRUs. If not a new one is added with
+#' the defined parameters (`plnt_com`, `lum_mgt`, `lum_cn2`, `lum_cpr`, and
+#' `lum_ovn`).
+#'
+#' @param swat_inputs List with SWAT+ input files.
+#' @param hru_id HRU IDs for which the land use is updated.
+#' @param type Type of the new land cover. This will be the new lan duse label
+#'   in the updated landuse.lum. `type` must be a single character string
+#' @param plnt_com The plant community which is assigned to the land use. A
+#'   single character string must be provided. The passed value must be defined
+#'   in the 'plant.ini' intput file. If (default) `NULL`, then 'plnt_com' will
+#'   be set 'null' in the landuse.lum.
+#' @param lum_mgt The management which is assigned to the land use. A single
+#'   character string must be provided. The passed value must be defined in the
+#'   'management.sch' intput file. If (default) `NULL`, then 'mgt' will be set
+#'   'null' in the landuse.lum.
+#' @param lum_cn2 The cn2 parameter set which is assigned to the land use. A
+#'   single character string must be provided. The passed value must be defined
+#'   in the 'cntabe.lum' intput file. If (default) `NULL`, then 'cn2' will be
+#'   set 'null' in the landuse.lum.
+#' @param lum_cpr The conservation practice which is assigned to the land use. A
+#'   single character string must be provided. The passed value must be defined
+#'   in the 'cons_practice.lum' intput file. If (default) `NULL`, then
+#'   'cons_prac' will be set 'null' in the landuse.lum.
+#' @param lum_ovn The Mannings n parametrization which is assigned to the land
+#'   use. A single character string must be provided. The passed value must be
+#'   defined in the 'ovn_table.lum' intput file. If (default) `NULL`, then
+#'   'ov_mann' will be set 'null' in the landuse.lum.
+#'
+#' @returns The SWAT+ input tables list with the updated landuse.lum and
+#'   hru-data.hru input tables.
+#'
+#' @importFrom dplyr select %>%
+#' @importFrom purrr map map_df map_lgl
+#' @importFrom stringr str_detect str_remove_all
+#' @importFrom tibble add_row
+#' @importFrom tidyr replace_na
+#'
+#' @export
+#'
+update_landuse <- function(swat_inputs, hru_id, type,
+                           plnt_com = NULL, lum_mgt = NULL, lum_cn2  = NULL,
+                           lum_cpr  = NULL, lum_ovn = NULL) {
+  # General data type/structure checks
+  stopifnot(is.numeric(hru_id))
+  stopifnot(is.character(type))
+  stopifnot(length(type) == 1)
+  stopifnot(is.character(plnt_com) | is.null(plnt_com))
+  stopifnot(length(plnt_com) <= 1)
+  stopifnot(is.character(lum_mgt) | is.null(lum_mgt))
+  stopifnot(length(lum_mgt) <= 1)
+  stopifnot(is.character(lum_cn2) | is.null(lum_cn2))
+  stopifnot(length(lum_cn2) <= 1)
+  stopifnot(is.character(lum_cpr) | is.null(lum_cpr))
+  stopifnot(length(lum_cpr) <= 1)
+  stopifnot(is.character(lum_ovn) | is.null(lum_ovn))
+  stopifnot(length(lum_ovn) <= 1)
+
+  # Checks for all inputs if they are available in the respective SWAT+ input
+  # files
+  if (! is.null(plnt_com)) {
+    if (!plnt_com %in% swat_inputs$plant.ini$pcom_name) {
+      stop("The 'plnt_com' = '", plnt_com, "' does not exist in 'plant.ini'.\n",
+           "Cannot update the land use for '", type, "' in the following HRUs:\n",
+           paste(hru_id, collapse = ', '))
+    }
+  } else {
+    plnt_com <- 'null'
+  }
+  if (! is.null(lum_mgt)) {
+    if (!lum_mgt %in% swat_inputs$management.sch$name) {
+      stop("The 'lum_mgt' = '", lum_mgt, "' does not exist in 'management.sch'.\n",
+           "Cannot update the land use for '", type, "' in the following HRUs:\n",
+           paste(hru_id, collapse = ', '))
+    }
+  } else {
+    lum_mgt <- 'null'
+  }
+  if (! is.null(lum_cn2)) {
+    if (!lum_cn2 %in% swat_inputs$cntabe.lum$name) {
+      stop("The 'lum_cn2' = '", lum_cn2, "' does not exist in 'cntable.lum'.\n",
+           "Cannot update the land use for '", type, "' in the following HRUs:\n",
+           paste(hru_id, collapse = ', '))
+    }
+  } else {
+    lum_cn2 <- 'null'
+  }
+  if (! is.null(lum_cpr)) {
+    if (!lum_cpr %in% swat_inputs$cons_practice.lum$name) {
+      stop("The 'lum_cn2' = '", lum_cn2, "' does not exist in 'cons_practice.lum'.\n",
+           "Cannot update the land use for '", type, "' in the following HRUs:\n",
+           paste(hru_id, collapse = ', '))
+    }
+  } else {
+    lum_cpr <- 'null'
+  }
+  if (! is.null(lum_ovn)) {
+    if (!lum_ovn %in% swat_inputs$ovn_table.lum$name) {
+      stop("The 'lum_ovn' = '", lum_ovn, "' does not exist in 'ovn_table.lum'.\n",
+           "Cannot update the land use for '", type, "' in the following HRUs:\n",
+           paste(hru_id, collapse = ', '))
+    }
+  } else {
+    lum_ovn <- 'null'
+  }
+
+  # Get all lum names from landuse.lum
+  lum_names <- swat_inputs$landuse.lum$name
+  # Find all alternative names with for the input argument 'type'
+  lum_alt <- lum_names[str_detect(lum_names,
+                                  paste0(type, '_lum|',
+                                         type, '[:digit:]+', '_lum'))]
+
+  # If type_lum or any of the alternatives are available check if their
+  # parameters match. Otherwise generate a new landuse.lum entry
+  if(length(lum_alt) > 0) {
+    # Get all lines for the lum alternatives from landuse.lum
+    lum_i <- swat_inputs$landuse.lum[swat_inputs$landuse.lum$name %in% lum_alt,]
+
+    # Compare the parameters of the alternatives to the input arguments
+    lum_upd_par   <- c(plnt_com, lum_mgt, lum_cn2, lum_cpr, lum_ovn)
+    # Check if all are inputs are identical with the parameters in one of
+    # the selected rows from landuse.lum
+    lum_par_ident <- lum_i %>%
+      split(., 1:nrow(.)) %>%
+      map(., ~ select(.x, plnt_com, mgt, cn2, cons_prac, ov_mann)) %>%
+      map(., ~ unlist(.x)) %>%
+      map_lgl(., ~ all(.x == lum_upd_par))
+
+    # If non of the landuse.lum lines do have idntical parameters to the input
+    # arguments create a new entry in landuse.lum
+    if (! any(lum_par_ident)) {
+      # Identify maximum ID which was assigned to this type
+      id_max <- lum_alt %>%
+        str_remove_all(., paste0(type, '_lum')) %>%
+        as.numeric(.) %>%
+        c(0,.) %>%
+        max(., na.rm = TRUE)
+
+      # Create new lum label
+      lum_lbl <- paste0(type, id_max+1, '_lum')
+
+      # Add entry in landuse lum with the new name and parameters
+      swat_inputs$landuse.lum <- swat_inputs$landuse.lum %>%
+        add_row(., name = lum_lbl,
+                   plnt_com = plnt_com,
+                   mgt = lum_mgt,
+                   cn2 = lum_cn2,
+                   ov_mann = lum_ovn) %>%
+      map_df(., ~replace_na(.x, 'null'))
+    } else {
+      # Use the first identical landuse to assign to the HRUs
+      lum_lbl <- lum_alt[lum_par_ident][1]
+    }
+  } else {
+    lum_lbl <- paste0(type, '_lum')
+    swat_inputs$landuse.lum <- swat_inputs$landuse.lum %>%
+      add_row(., name = lum_lbl,
+              plnt_com = plnt_com,
+              mgt = lum_mgt,
+              cn2 = lum_cn2,
+              ov_mann = lum_ovn) %>%
+      map_df(., ~replace_na(.x, 'null'))
+  }
+
+  # Assign updated land use to the respective HRUs in hru-data.hru
+  swat_inputs$hru_data.hru$lu_mgt[swat_inputs$hru_data.hru$id %in% hru_id] <-
+    lum_lbl
+
+  return(swat_inputs)
+}
